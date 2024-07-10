dfdfb0c20e21f0da8ccab539449a377406add106
申志远 <shenzhiyuan.hom@bytedance.com>
2024-07-10 20:27:31 +0800

    first commit

---
+++ src/main/java/io/uikhsoroy/esg/insight/EsgInsightApplication.java
@@ -0,0 +1,13 @@
+package io.uikhsoroy.esg.insight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/uikhsoroy/esg/insight/EsgInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package io.uikhsoroy.esg.insight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
